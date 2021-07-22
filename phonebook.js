f97f434248921c7a4813998ce45cdcfec091074e
lghiggino <lghiggino@gmail.com>
2021-07-21 21:35:19 -0300

    3.2 is done

--- phonebook.js
+++ phonebook.js
@@ -13,6 +13,18 @@ app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
+app.get("/info", (request, response) => {
+    const personsLenght = persons.length
+    const newDate = new Date()
+
+    response.send(`
+        <div>
+            <h2>Phonebook has info for ${personsLenght} people</h2>
+            <p> ${newDate} </p>
+        </div>
+    `)
+})
+
 const PORT = 3002
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
